5a5853f631ee2ad716d93a283cf5947560c894ad
handballnut <member@JMAC.local>
2014-08-04 15:54:12 -0700

    added1stunityproject

---
+++ New Unity Project/Assets/scripts/BoxController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class BoxController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Translate(0.2f, 0f, 0f);
+	
+	}
+	void MyFunction(){
+	}
